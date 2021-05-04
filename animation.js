ae417c66813f3b9b26cfcd798c18978c7b1d7f95
ossama zaoute <ramziioussama@gmail.com>
2021-05-04 16:10:00 +0000

    update

--- animation.js
+++ animation.js
@@ -4,10 +4,12 @@ gsap
     scrollTrigger: {
       trigger: ".hero",
       start: "top top",
-      end: "top top",
+      end: "top center",
       scrub: true,
     },
   })
-  .to(".top_bun", { yPercent: -20 }, 0)
-  .to(".meat", { width: "110%" }, 0)
-  .to(".bottom_bun", { yPercent: 30 }, 0);
+  .to(".top_bun", { yPercent: -30 }, 0)
+  .to(".meat", { width: "109%" }, 0)
+  .to(".bottom_bun", { yPercent: 40 }, 0)
+  .to(".pepper_img", { yPercent: -10 }, 0)
+  .to(".salt_img", { yPercent: -40 }, 0);
